081c77c81e085be72dfee09c2852651f4b88470a
JayusJay <atayure@gmail.com>
2023-06-17 12:24:35 +0000

    ERC1400 review

--- src/ERC1400/ERC1400.sol
+++ src/ERC1400/ERC1400.sol
@@ -1,6 +1,11 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import { IERC1400 } from "./IERC1400.sol";
+
+//TODO: implement ERC1400. Inherit subcontracts and override or implement from scratch?
+
+//TODO: implement non-fungible version of ERC1400.
 contract ERC1400 {
 	///@dev the complete security token standard.
 }
--- src/ERC1400/IERC1400.sol
+++ src/ERC1400/IERC1400.sol
@@ -1,4 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IERC1400 {}
+import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { IERC1643 } from "../ERC1643/IERC1643.sol";
+
+interface IERC1400 is IERC20, IERC1643 {}
