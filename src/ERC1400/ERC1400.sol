f09cb70e7ba13c679fc8dd8624c0a891578a4004
JayusJay <atayure@gmail.com>
2023-06-24 16:17:46 +0000

    QA

--- src/ERC1400/ERC1400.sol
+++ src/ERC1400/ERC1400.sol
@@ -1,11 +1,15 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {Ownable2Step} from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
 import { IERC1400 } from "./IERC1400.sol";
 
 //TODO: implement ERC1400. Inherit subcontracts and override or implement from scratch?
 
 //TODO: implement non-fungible version of ERC1400.
-contract ERC1400 {
-	///@dev the complete security token standard.
+contract ERC1400 is Ownable2Step{
+	/**
+	 * @dev tokens not belonging to any partition should use this partition
+	 */
+	bytes32 public constant DEFAULT_PARTITION = bytes32(0);
 }
