b63e0ae4f351dc9d4108e6cebfa26161abb8b922
JayusJay <atayure@gmail.com>
2023-07-22 12:45:28 +0000

    setting up ERC1400 NFT

--- src/ERC1400/ERC1400NFT.sol
+++ src/ERC1400/ERC1400NFT.sol
@@ -2,11 +2,73 @@
 pragma solidity ^0.8.0;
 
 /**
- * @dev ERC1400 compatible with ERC721 for non-fungible security tokens
+ * @dev ERC1400 compatible with ERC721 for non-fungible security tokens.
+ * @dev Each token Id must be unique irrespective of partition.
+ * @dev A token id issued to the default partition cannot be issued to any other partition.
  */
 contract ERC1400NFT {
-    
-    function tokenDetails(uint256 tokenId) public pure virtual returns (string memory) {
-        return string(abi.encodePacked("tokenId:", tokenId));
-    }
+	///@dev Default token partition
+	bytes32 public constant DEFAULT_PARTITION = bytes32(0);
+
+	///@dev token name
+	string private _name;
+
+	///@dev token symbol
+	string private _symbol;
+
+	///@dev token contract version for EIP712
+	string private _version;
+
+	///@dev should track if token is issuable or not. Should not be modifiable if false.
+	bool private _isIssuable;
+
+	///@dev array of token partitions.
+	bytes32[] private _partitions;
+
+	///@dev array of token controllers.
+	address[] private _controllers;
+
+	///@dev mapping from token ID to owner address
+	mapping(uint256 => address) private _owners;
+
+	///@dev mapping owner address to token count
+	mapping(address => uint256) private _balances;
+
+	///@dev mapping of owner to partition to token count
+	mapping(address => mapping(bytes32 => uint256)) private _balancesByPartition;
+
+	///@dev mapping of partition to index in _partitions array.
+	mapping(bytes32 => uint256) private _partitionIndex;
+
+	///@dev mapping of controller to index in _controllers array.
+	mapping(address => uint256) private _controllerIndex;
+
+	///@dev mapping from user to array of partitions.
+	mapping(address => bytes32[]) private _partitionsOf;
+
+	///@dev mapping of user to mapping of partition in _partitionsOf array to index of partition in this array.
+	mapping(address => mapping(bytes32 => uint256)) private _partitionIndexOfUser;
+
+	///@dev mapping from token ID to approved address
+	mapping(uint256 => address) private _tokenApprovals;
+
+	///@dev mapping from owner to operator approvals
+	mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+	///@dev mapping from owner to partition to operator approvals
+	mapping(address => mapping(bytes32 => mapping(address => bool))) private _operatorApprovalsByPartition;
+
+	///@dev mapping of used nonces
+	mapping(address => uint256) private _userNonce;
+
+	constructor(string memory name_, string memory symbol_, string memory version_) {
+		_name = name_;
+		_symbol = symbol_;
+		_version = version_;
+		_isIssuable = true;
+	}
+
+	function tokenDetails(uint256 tokenId) public pure virtual returns (string memory) {
+		return string(abi.encodePacked("tokenId:", tokenId));
+	}
 }
---
+++ src/ERC1400/IERC1400NFT.sol
@@ -0,0 +1,152 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { IERC1643 } from "../ERC1643/IERC1643.sol";
+
+interface IERC1400NFT is IERC1643 {
+	event AuthorizedOperator(address indexed operator, address indexed tokenHolder);
+	event RevokedOperator(address indexed operator, address indexed tokenHolder);
+	event AuthorizedOperatorByPartition(
+		bytes32 indexed partition,
+		address indexed operator,
+		address indexed tokenHolder
+	);
+	event RevokedOperatorByPartition(bytes32 indexed partition, address indexed operator, address indexed tokenHolder);
+
+	// Issuance / Redemption Events
+	event Issued(address indexed operator, address indexed to, uint256 tokenId, bytes data);
+	event Redeemed(address indexed operator, address indexed from, uint256 tokenId, bytes data);
+	event IssuedByPartition(bytes32 indexed partition, address indexed to, uint256 tokenId, bytes data);
+	event RedeemedByPartition(
+		bytes32 indexed partition,
+		address indexed operator,
+		address indexed from,
+		uint256 tokenId,
+		bytes data,
+		bytes operatorData
+	);
+
+	// Token Information
+	function isIssuable() external view returns (bool);
+
+	function balanceOf(address account) external view returns (uint256);
+
+	function ownerOf(uint256 tokenId) external view returns (address);
+
+	function balanceOfByPartition(bytes32 partition, address account) external view returns (uint256);
+
+	function partitionsOf(address account) external view returns (bytes32[] memory);
+
+	// Token Transfers
+	function transferByPartition(
+		bytes32 partition,
+		address to,
+		uint256 tokenId,
+		bytes calldata data
+	) external returns (bytes32);
+
+	function operatorTransferByPartition(
+		bytes32 partition,
+		address from,
+		address to,
+		uint256 tokenId,
+		bytes calldata data,
+		bytes calldata operatorData
+	) external returns (bytes32);
+
+	function controllerTransfer(
+		address from,
+		address to,
+		uint256 tokenId,
+		bytes calldata data,
+		bytes calldata operatorData
+	) external;
+
+	function transferWithData(address to, uint256 tokenId, bytes memory data) external;
+
+	function transferFromWithData(address from, address to, uint256 tokenId, bytes memory data) external;
+
+	// Transfer Validity
+	function canTransferByPartition(
+		address from,
+		address to,
+		bytes32 partition,
+		uint256 tokenId,
+		bytes calldata data
+	) external view returns (bytes memory, bytes32, bytes32);
+
+	function canTransfer(
+		address to,
+		uint256 tokenId,
+		bytes memory data
+	) external view returns (bool, bytes memory, bytes32);
+
+	function canTransferFrom(
+		address from,
+		address to,
+		uint256 tokenId,
+		bytes memory data
+	) external view returns (bool, bytes memory, bytes32);
+
+	// Operator Information
+	function isOperator(address operator, address account) external view returns (bool);
+
+	function isOperatorForPartition(bytes32 partition, address operator, address account) external view returns (bool);
+
+	// Controller Information
+	function isControllable() external view returns (bool);
+
+	// Operator Management
+	function authorizeOperator(address operator) external;
+
+	function revokeOperator(address operator) external;
+
+	function authorizeOperatorByPartition(bytes32 partition, address operator) external;
+
+	function revokeOperatorByPartition(bytes32 partition, address operator) external;
+
+	// Issuance / Redemption
+	function issue(address tokenHolder, uint256 tokenId, bytes memory data) external;
+
+	function issueByPartition(bytes32 partition, address account, uint256 tokenId, bytes calldata data) external;
+
+	function redeem(uint256 tokenId, bytes memory data) external;
+
+	function redeemFrom(address tokenHolder, uint256 tokenId, bytes memory data) external;
+
+	function redeemByPartition(bytes32 partition, uint256 tokenId, bytes calldata data) external;
+
+	function operatorRedeemByPartition(
+		bytes32 partition,
+		address account,
+		uint256 tokenId,
+		bytes calldata data,
+		bytes calldata operatorData
+	) external;
+
+	// Controller Operation
+	function controllerRedeem(
+		address tokenHolder,
+		uint256 tokenId,
+		bytes calldata data,
+		bytes calldata operatorData
+	) external;
+
+	// Controller Events
+	event ControllerTransfer(
+		address indexed controller,
+		address indexed from,
+		address indexed to,
+		uint256 tokenId,
+		bytes data,
+		bytes operatorData
+	);
+
+	event ControllerRedemption(
+		address indexed controller,
+		address indexed tokenHolder,
+		uint256 tokenId,
+		bytes data,
+		bytes operatorData
+	);
+}
