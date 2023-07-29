2fedcad2b851653f83dafecd31a692899bc19a21
JayusJay <atayure@gmail.com>
2023-07-29 12:35:33 +0000

    ERC1400NFT: upcoming

--- src/ERC1400/ERC1400NFT.sol
+++ src/ERC1400/ERC1400NFT.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import { Ownable2Step } from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+import { Context } from "openzeppelin-contracts/contracts/utils/Context.sol";
 import { ERC165 } from "openzeppelin-contracts/contracts/utils/introspection/ERC165.sol";
 import { Strings } from "openzeppelin-contracts/contracts/utils/Strings.sol";
 import { IERC1400NFT } from "./IERC1400NFT.sol";
@@ -10,7 +11,9 @@ import { IERC1400NFT } from "./IERC1400NFT.sol";
  * @dev Each token Id must be unique irrespective of partition.
  * @dev A token id issued to the default partition cannot be issued to any other partition.
  */
-contract ERC1400NFT is Ownable2Step, ERC165 {
+
+///@dev use OZ's Context contract
+contract ERC1400NFT is Context, Ownable2Step, ERC165 {
 	using Strings for uint256;
 	///@dev Default token partition
 	bytes32 public constant DEFAULT_PARTITION = bytes32(0);
@@ -126,7 +129,10 @@ contract ERC1400NFT is Ownable2Step, ERC165 {
 	 */
 	function balanceOfByPartition(bytes32 partition, address account) public view virtual returns (uint256) {
 		require(account != address(0), "ERC1400NFT: balance query for the zero address");
-		require(_partitionIndex[partition] != 0 || partition == DEFAULT_PARTITION, "ERC1400NFT: inexistent partition"); //!recheck
+		require(
+			_partitions[_partitionIndex[partition]] == partition || partition == DEFAULT_PARTITION,
+			"ERC1400NFT: nonexistent partition"
+		);
 		return _balancesByPartition[account][partition];
 	}
 
@@ -139,8 +145,14 @@ contract ERC1400NFT is Ownable2Step, ERC165 {
 		return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
 	}
 
-	function changeBaseURI(string memory baseUri_) public virtual onlyOwner {
+	function _changeBaseURI(string memory baseUri_) internal virtual onlyOwner {
 		_baseUri = baseUri_;
+	}
+
+	function ownerOf(uint256 tokenId) public view virtual returns (address) {
+		address owner = _ownerOf(tokenId);
+		require(owner != address(0), "ERC1400NFT: invalid token ID");
+		return owner;
 	}
 
 	/**
@@ -157,5 +169,35 @@ contract ERC1400NFT is Ownable2Step, ERC165 {
 	 */
 	function exists(uint256 tokenId) public view virtual returns (bool) {
 		return _ownerOf(tokenId) != address(0);
+	}
+
+	/**
+	 * @dev See {IERC721-isApprovedForAll}.
+	 */
+	function isApprovedForAll(address owner, address operator) public view virtual returns (bool) {
+		return _operatorApprovals[owner][operator];
+	}
+
+	function approve(address to, uint256 tokenId) public virtual {
+		address owner = _ownerOf(tokenId);
+		require(to != owner, "ERC1400NFT: approval to current owner");
+
+		require(
+			_msgSender() == owner || isApprovedForAll(owner, _msgSender()),
+			"ERC1400NFT: approve caller is not token owner or approved for all"
+		);
+
+		_approve(to, tokenId);
+	}
+
+	/**
+	 * @dev Approve `to` to operate on `tokenId`
+	 *
+	 * Emits an {Approval} event.
+	 */
+	function _approve(address to, uint256 tokenId) internal virtual {
+		_tokenApprovals[tokenId] = to;
+
+		//emit Approval(ownerOf(tokenId), to, tokenId);
 	}
 }
