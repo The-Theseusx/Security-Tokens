24dbb7d6976dbf283e4a29f5f16bfb1e622c6d61
JayusJay <atayure@gmail.com>
2023-09-18 17:33:43 +0000

    feat!: Incomplete - testing signature verification with token redemption

--- test/ERC1400/ERC1400Redemption.t.sol
+++ test/ERC1400/ERC1400Redemption.t.sol
@@ -1,26 +1,38 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { Test } from "forge-std/Test.sol";
-import { ERC1400TestStorage } from "./utils/ERC1400TestStorage.sol";
-import { ERC1400TestErrors } from "./utils/ERC1400TestErrors.sol";
+import { Test, console } from "forge-std/Test.sol";
+import { ERC1400BaseTest } from "./ERC1400BaseTest.t.sol";
+import { ERC1400SigUtils } from "./utils/ERC1400SigUtils.sol";
 
-abstract contract ERC1400RedemptionTest is Test, ERC1400TestStorage, ERC1400TestErrors {
+abstract contract ERC1400RedemptionTest is ERC1400BaseTest, ERC1400SigUtils {
 	function testRedemptionShouldFailWhenNotRedeemer() public {
-		string memory errMsg = accessControlError(address(this), ERC1400MockToken.ERC1400_REDEEMER_ROLE());
+		// string memory errMsg = accessControlError(address(this), ERC1400MockToken.ERC1400_REDEEMER_ROLE());
+
+		///@dev @notice bad signer used
+		bytes memory validationData = prepareRedemptionSignature(999, DEFAULT_PARTITION, tokenAdmin, 100e18, 0, 0);
 
 		///@dev mock owner because they have tokens.
-		vm.startPrank(OWNER);
-		vm.expectRevert(bytes(errMsg));
-		ERC1400MockToken.redeem(100, "");
+		vm.startPrank(tokenAdmin);
+		vm.expectRevert("ERC1400: Invalid data");
+		ERC1400MockToken.redeem(100e18, validationData);
 		vm.stopPrank();
 	}
 
-	// ///@dev start  necessary prank before calling this function
-	// function redeemTokens(bytes32 partition, address from, uint256 amount, bytes memory data) internal {
-	// 	if (from != address(0)) {
-	// 		if (partition == DEFAULT_PARTITION) ERC1400MockToken.redeem(amount, data);
-	// 		else ERC1400MockToken.redeemByPartition(partition, amount, data);
-	// 	} else ERC1400MockToken.redeemFrom(from, amount, data);
+	// function testRedemptionAuthorizedByTokenRedeemer() public {
+	// 	bytes memory validationData = prepareRedemptionSignature(
+	// 		TOKEN_REDEEMER_PK,
+	// 		DEFAULT_PARTITION,
+	// 		tokenAdmin,
+	// 		100e18,
+	// 		0,
+	// 		0
+	// 	);
+	// 	address redeemer = vm.addr(TOKEN_REDEEMER_PK);
+	// 	console.log("Signer: ", redeemer);
+	// 	vm.startPrank(tokenAdmin);
+	// 	// vm.expectRevert("ERC1400: Invalid data");
+	// 	ERC1400MockToken.redeem(100e18, validationData);
+	// 	vm.stopPrank();
 	// }
 }
