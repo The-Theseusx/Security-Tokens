cacc1b4cb7f0dd17a08775f266c7d840440eaa41
JayusJay <atayure@gmail.com>
2023-09-18 17:31:10 +0000

    feat: util library for signatures

---
+++ test/utils/SigUtils.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ERC1400ValidateDataParams, ERC1400NFTValidateDataParams } from "../../src/utils/DataTypes.sol";
+
+contract SigUtils {
+	bytes32 internal immutable _domainSeparator; ///@dev EIP712 domain separator.
+	bytes32 internal immutable _msgTypeHash; ///@dev eg keccak256("ValidateData(address from,address to,uint256 amount,bytes32 data)");
+
+	constructor(bytes32 domainSeparator, bytes32 msgTypeHash) {
+		_domainSeparator = domainSeparator;
+		_msgTypeHash = msgTypeHash;
+	}
+
+	///@dev computes the hash of a ERC1400 data validation struct
+	function _getERC1400StructHash(
+		ERC1400ValidateDataParams memory _validateDataParams,
+		uint256 _nonce,
+		uint48 _deadline
+	) internal view returns (bytes32) {
+		return
+			keccak256(
+				abi.encode(
+					_msgTypeHash,
+					_validateDataParams.from,
+					_validateDataParams.to,
+					_validateDataParams.amount,
+					_validateDataParams.partition,
+					_nonce,
+					_deadline
+				)
+			);
+	}
+
+	///@dev computes the hash of a ERC1400NFT data validation struct
+	function _getERC1400NFTStructHash(
+		ERC1400NFTValidateDataParams memory _validateDataParams,
+		uint256 _nonce,
+		uint48 _deadline
+	) internal view returns (bytes32) {
+		return
+			keccak256(
+				abi.encode(
+					_msgTypeHash,
+					_validateDataParams.from,
+					_validateDataParams.to,
+					_validateDataParams.tokenId,
+					_validateDataParams.partition,
+					_nonce,
+					_deadline
+				)
+			);
+	}
+
+	///@dev computes the hash of the fully encoded EIP-712 message for the domain, which can be used to recover the signer
+	function getERC1400TypedDataHash(
+		ERC1400ValidateDataParams memory _validateDataParams,
+		uint256 nonce,
+		uint48 deadline
+	) public view returns (bytes32) {
+		return
+			keccak256(
+				abi.encodePacked(
+					"\x19\x01",
+					_domainSeparator,
+					_getERC1400StructHash(_validateDataParams, nonce, deadline)
+				)
+			);
+	}
+
+	///@dev computes the hash of the fully encoded EIP-712 message for the domain of ERC1400NFT token, which can be used to recover the signer
+	function getERC1400NFTTypedDataHash(
+		ERC1400NFTValidateDataParams memory _validateDataParams,
+		uint256 nonce,
+		uint48 deadline
+	) public view returns (bytes32) {
+		return
+			keccak256(
+				abi.encodePacked(
+					"\x19\x01",
+					_domainSeparator,
+					_getERC1400NFTStructHash(_validateDataParams, nonce, deadline)
+				)
+			);
+	}
+}
