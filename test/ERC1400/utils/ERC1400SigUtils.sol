4450505ac03ec56bfbacd1ab0d8945f045554515
JayusJay <atayure@gmail.com>
2023-09-19 12:53:46 +0000

    chore!: restructured file

--- test/ERC1400/utils/ERC1400SigUtils.sol
+++ test/ERC1400/utils/ERC1400SigUtils.sol
@@ -5,6 +5,17 @@ import { ERC1400TestStorage } from "./ERC1400TestStorage.sol";
 import { ERC1400ValidateDataParams } from "../../../src/utils/DataTypes.sol";
 
 abstract contract ERC1400SigUtils is ERC1400TestStorage {
+	struct PrepareSignatureParams {
+		uint256 signerPk;
+		bytes32 authorizerRole;
+		bytes32 partition;
+		address from;
+		address to;
+		uint256 amount;
+		uint256 nonce;
+		uint48 deadline;
+	}
+
 	function prepareTransferSignature(
 		uint256 signerPk,
 		bytes32 partition,
@@ -13,7 +24,24 @@ abstract contract ERC1400SigUtils is ERC1400TestStorage {
 		uint256 amount,
 		uint256 nonce,
 		uint48 deadline
-	) public {}
+	) public returns (bytes memory) {
+		bytes32 role = ERC1400MockToken.ERC1400_TRANSFER_AGENT_ROLE();
+		nonce = nonce == 0 ? ERC1400MockToken.getRoleNonce(role) : nonce;
+		deadline = deadline == 0 ? uint48(block.timestamp + 1 minutes) : deadline;
+
+		PrepareSignatureParams memory sigParams = PrepareSignatureParams({
+			signerPk: signerPk,
+			authorizerRole: role,
+			partition: partition,
+			from: from,
+			to: to,
+			amount: amount,
+			nonce: nonce,
+			deadline: deadline
+		});
+
+		return _prepareSignature(sigParams);
+	}
 
 	function prepareRedemptionSignature(
 		uint256 signerPk,
@@ -23,30 +51,48 @@ abstract contract ERC1400SigUtils is ERC1400TestStorage {
 		uint256 nonce,
 		uint48 deadline
 	) public returns (bytes memory) {
-		bytes32 role_ = ERC1400MockToken.ERC1400_REDEEMER_ROLE();
-		nonce = nonce == 0 ? ERC1400MockToken.getRoleNonce(role_) : nonce;
+		bytes32 role = ERC1400MockToken.ERC1400_REDEEMER_ROLE();
+		nonce = nonce == 0 ? ERC1400MockToken.getRoleNonce(role) : nonce;
 		deadline = deadline == 0 ? uint48(block.timestamp + 1 minutes) : deadline;
 
-		ERC1400ValidateDataParams memory validationData = ERC1400ValidateDataParams({
-			authorizerRole: role_,
+		PrepareSignatureParams memory sigParams = PrepareSignatureParams({
+			signerPk: signerPk,
+			authorizerRole: role,
+			partition: partition,
 			from: from,
 			to: address(0),
 			amount: amount,
-			partition: partition,
+			nonce: nonce,
+			deadline: deadline
+		});
+
+		return _prepareSignature(sigParams);
+	}
+
+	function _prepareSignature(PrepareSignatureParams memory _sigParams) internal returns (bytes memory) {
+		ERC1400ValidateDataParams memory validationData = ERC1400ValidateDataParams({
+			authorizerRole: _sigParams.authorizerRole,
+			from: _sigParams.from,
+			to: _sigParams.to,
+			amount: _sigParams.amount,
+			partition: _sigParams.partition,
 			data: ""
 		});
 
 		///@dev data field in ERC1400ValidateDataParams not needed in this step hence "" is used
-		bytes32 structMessage = sigUtilsContract.getERC1400TypedDataHash(validationData, nonce, deadline);
+		bytes32 structMessage = sigUtilsContract.getERC1400TypedDataHash(
+			validationData,
+			_sigParams.nonce,
+			_sigParams.deadline
+		);
 
-		address signer = vm.addr(signerPk);
+		address signer = vm.addr(_sigParams.signerPk);
 
 		vm.startPrank(signer);
-		(uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPk, structMessage);
+		(uint8 v, bytes32 r, bytes32 s) = vm.sign(_sigParams.signerPk, structMessage);
 		bytes memory signature = abi.encodePacked(r, s, v);
 		vm.stopPrank();
 
-		bytes memory data = abi.encode(signature, deadline);
-		return data;
+		return abi.encode(signature, _sigParams.deadline);
 	}
 }
