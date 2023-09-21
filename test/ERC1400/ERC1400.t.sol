2eba35019fd4d019832dd6d6e26441e5e657c6c3
JayusJay <atayure@gmail.com>
2023-09-21 19:37:55 +0000

    chore: Added operator tests

--- test/ERC1400/ERC1400.t.sol
+++ test/ERC1400/ERC1400.t.sol
@@ -108,4 +108,96 @@ contract ERC1400Test is ERC1400IssuanceTest, ERC1400RedemptionTest {
 			"aliceOperator should be an operator for shared spaces partition"
 		);
 	}
+
+	function testShouldRevokeOperator() public {
+		vm.startPrank(alice);
+		ERC1400MockToken.authorizeOperator(aliceOperator);
+
+		skip(1 minutes);
+
+		vm.expectEmit(true, true, true, false);
+		emit RevokedOperator(aliceOperator, alice);
+
+		ERC1400MockToken.revokeOperator(aliceOperator);
+
+		assertFalse(ERC1400MockToken.isOperator(aliceOperator, alice), "aliceOperator should be revoked");
+		vm.stopPrank();
+	}
+
+	function testShouldRevokeAllOperatorsOfUser() public {
+		vm.startPrank(bob);
+		ERC1400MockToken.authorizeOperator(aliceOperator);
+		ERC1400MockToken.authorizeOperator(bobOperator);
+		ERC1400MockToken.authorizeOperatorByPartition(DEFAULT_PARTITION, tokenAdminOperator);
+		ERC1400MockToken.authorizeOperatorByPartition(SHARED_SPACES_PARTITION, notTokenAdminOperator);
+		vm.stopPrank();
+
+		assertTrue(ERC1400MockToken.isOperator(aliceOperator, bob), "aliceOperator should be an operator for Bob");
+		assertTrue(ERC1400MockToken.isOperator(bobOperator, bob), "bobOperator should be an operator for Bob");
+		assertTrue(
+			ERC1400MockToken.isOperatorForPartition(DEFAULT_PARTITION, tokenAdminOperator, bob),
+			"tokenAdminOperator should be an operator of the default partition for Bob"
+		);
+		assertTrue(
+			ERC1400MockToken.isOperatorForPartition(SHARED_SPACES_PARTITION, notTokenAdminOperator, bob),
+			"notTokenAdminOperator should be an operator of shared spaces partition for Bob"
+		);
+
+		address[] memory operators = new address[](4);
+		operators[0] = bobOperator;
+		operators[1] = aliceOperator;
+		operators[2] = tokenAdminOperator;
+		operators[3] = notTokenAdminOperator;
+
+		vm.startPrank(bob);
+		ERC1400MockToken.revokeOperators(operators);
+		vm.stopPrank();
+
+		assertFalse(ERC1400MockToken.isOperator(aliceOperator, bob), "aliceOperator should not be an operator for Bob");
+		assertFalse(ERC1400MockToken.isOperator(bobOperator, bob), "bobOperator should not be an operator for Bob");
+		assertFalse(
+			ERC1400MockToken.isOperatorForPartition(DEFAULT_PARTITION, tokenAdminOperator, bob),
+			"tokenAdminOperator should not be an operator of the default partition for Bob"
+		);
+		assertFalse(
+			ERC1400MockToken.isOperatorForPartition(SHARED_SPACES_PARTITION, notTokenAdminOperator, bob),
+			"notTokenAdminOperator should not be an operator of shared spaces partition for Bob"
+		);
+	}
+
+	function testShouldRevokeOperatorsForDefaultPartitionOnly() public {
+		///@dev @notice notTokenAdmin has no tokens and no partitions.
+		vm.startPrank(notTokenAdmin);
+		ERC1400MockToken.authorizeOperatorByPartition(DEFAULT_PARTITION, tokenAdminOperator);
+		ERC1400MockToken.authorizeOperatorByPartition(DEFAULT_PARTITION, notTokenAdminOperator);
+		vm.stopPrank();
+
+		assertTrue(
+			ERC1400MockToken.isOperatorForPartition(DEFAULT_PARTITION, tokenAdminOperator, notTokenAdmin),
+			"tokenAdminOperator should be an operator of the default partition for notTokenAdmin"
+		);
+
+		assertTrue(
+			ERC1400MockToken.isOperatorForPartition(DEFAULT_PARTITION, notTokenAdminOperator, notTokenAdmin),
+			"notTokenAdminOperator should be an operator of the default partition for notTokenAdmin"
+		);
+
+		address[] memory operators = new address[](2);
+		operators[0] = tokenAdminOperator;
+		operators[1] = notTokenAdminOperator;
+
+		vm.startPrank(notTokenAdmin);
+		ERC1400MockToken.revokeOperators(operators);
+		vm.stopPrank();
+
+		assertFalse(
+			ERC1400MockToken.isOperatorForPartition(DEFAULT_PARTITION, tokenAdminOperator, notTokenAdmin),
+			"tokenAdminOperator should not be an operator of the default partition for notTokenAdmin"
+		);
+
+		assertFalse(
+			ERC1400MockToken.isOperatorForPartition(DEFAULT_PARTITION, notTokenAdminOperator, notTokenAdmin),
+			"notTokenAdminOperator should not be an operator of the default partition for notTokenAdmin"
+		);
+	}
 }
