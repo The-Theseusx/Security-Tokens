2f03a49cedbf384bfbf03575e9a3552f24cb09a9
JayusJay <atayure@gmail.com>
2023-09-21 16:19:18 +0000

    chore: added operator tests

--- test/ERC1400/ERC1400.t.sol
+++ test/ERC1400/ERC1400.t.sol
@@ -29,4 +29,83 @@ contract ERC1400Test is ERC1400IssuanceTest, ERC1400RedemptionTest {
 		ERC1400MockToken.disableIssuance();
 		vm.stopPrank();
 	}
+
+	function testShouldDisableIssuanceWhenAdmin() public {
+		vm.startPrank(tokenAdmin);
+		ERC1400MockToken.disableIssuance();
+		vm.stopPrank();
+
+		assertFalse(ERC1400MockToken.isIssuable(), "Token should not be issuable");
+	}
+
+	function testShouldNotApproveSenderAsOperator() public {
+		vm.startPrank(alice);
+		vm.expectRevert("ERC1400: self authorization not allowed");
+		ERC1400MockToken.authorizeOperator(alice);
+		vm.stopPrank();
+	}
+
+	function testShouldApproveOperator() public {
+		vm.startPrank(alice);
+
+		vm.expectEmit(true, true, true, false);
+		emit AuthorizedOperator(aliceOperator, alice);
+
+		ERC1400MockToken.authorizeOperator(aliceOperator);
+		vm.stopPrank();
+
+		assertTrue(ERC1400MockToken.isOperator(aliceOperator, alice), "aliceOperator should be an operator");
+	}
+
+	function testShouldNotApproveSenderAsOperatorByPartition() public {
+		vm.startPrank(alice);
+		vm.expectRevert("ERC1400: self authorization not allowed");
+		ERC1400MockToken.authorizeOperatorByPartition(SHARED_SPACES_PARTITION, alice);
+		vm.stopPrank();
+	}
+
+	function testShouldApproveOperatorByPartition() public {
+		vm.startPrank(alice);
+
+		vm.expectEmit(true, true, true, true);
+		emit AuthorizedOperatorByPartition(SHARED_SPACES_PARTITION, aliceOperator, alice);
+
+		ERC1400MockToken.authorizeOperatorByPartition(SHARED_SPACES_PARTITION, aliceOperator);
+		vm.stopPrank();
+
+		assertFalse(
+			ERC1400MockToken.isOperator(aliceOperator, alice),
+			"aliceOperator should not be an operator but operator of shared spaces Partition"
+		);
+		assertFalse(
+			ERC1400MockToken.isOperatorForPartition(DEFAULT_PARTITION, aliceOperator, alice),
+			"aliceOperator should be an operator for the default partition"
+		);
+		assertTrue(
+			ERC1400MockToken.isOperatorForPartition(SHARED_SPACES_PARTITION, aliceOperator, alice),
+			"aliceOperator should be an operator for shared spaces partition"
+		);
+	}
+
+	function testShouldApproveOperatorForDefaultPartition() public {
+		vm.startPrank(bob);
+		vm.expectEmit(true, true, true, true);
+		emit AuthorizedOperatorByPartition(DEFAULT_PARTITION, bobOperator, bob);
+
+		ERC1400MockToken.authorizeOperatorByPartition(DEFAULT_PARTITION, bobOperator);
+		vm.stopPrank();
+
+		assertFalse(
+			ERC1400MockToken.isOperator(bobOperator, bob),
+			"aliceOperator should not be an operator but operator of shared spaces Partition"
+		);
+		assertFalse(
+			ERC1400MockToken.isOperatorForPartition(SHARED_SPACES_PARTITION, bobOperator, bob),
+			"aliceOperator should be an operator for the default partition"
+		);
+		assertTrue(
+			ERC1400MockToken.isOperatorForPartition(DEFAULT_PARTITION, bobOperator, bob),
+			"aliceOperator should be an operator for shared spaces partition"
+		);
+	}
 }
