808c93dbb80683c2d3d2947d74cceda179e60478
JayusJay <atayure@gmail.com>
2023-11-11 14:06:47 +0000

    feat: added canTransferByPartition tests

--- test/ERC1400/ERC1400CanTransfer.t.sol
+++ test/ERC1400/ERC1400CanTransfer.t.sol
@@ -1,11 +1,81 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import { console2 } from "forge-std/Test.sol";
+import { ERC1400 } from "../../src/ERC1400/ERC1400.sol";
 import { ERC1400BaseTest } from "./ERC1400BaseTest.t.sol";
 
 //solhint-disable func-name-mixedcase
 abstract contract ERC1400CanTransferTest is ERC1400BaseTest {
-	function testCanTransfer() public {}
+	function test_fuzz_canTransferByPartition(
+		address from,
+		address to,
+		bytes32 partition,
+		uint256 amount,
+		bytes memory data
+	) public {
+		vm.assume(from.code.length == 0);
+		vm.assume(to.code.length == 0);
+
+		if (partition < SHARED_SPACES_PARTITION) {
+			partition = SHARED_SPACES_PARTITION;
+		}
+
+		if (amount > 100e18 && from != address(0)) {
+			amount = ERC1400MockToken.balanceOfByPartition(SHARED_SPACES_PARTITION, alice);
+			vm.startPrank(alice);
+			ERC1400MockToken.transferByPartition(SHARED_SPACES_PARTITION, from, amount, "");
+			vm.stopPrank();
+		}
+
+		if (from != address(0) && partition == SHARED_SPACES_PARTITION) {
+			vm.startPrank(from);
+			ERC1400MockToken.approveByPartition(SHARED_SPACES_PARTITION, from, amount);
+			vm.stopPrank();
+		}
+
+		if (data.length > 0) {
+			data = prepareTransferSignature(TOKEN_TRANSFER_AGENT_PK, SHARED_SPACES_PARTITION, from, to, amount, 0, 0);
+		}
+
+		vm.startPrank(to);
+		(bytes memory statusCode, , ) = ERC1400MockToken.canTransferByPartition(from, to, partition, amount, data);
+		vm.stopPrank();
+
+		if (keccak256(statusCode) == keccak256(INVALID_SENDER)) {
+			console2.log("INVALID_SENDER");
+			assertTrue(from == address(0), "ERC1400: transferByPartition from the zero address should fail");
+		} else if (keccak256(statusCode) == keccak256(INVALID_RECEIVER)) {
+			console2.log("INVALID_RECEIVER");
+			assertTrue(to == address(0), "ERC1400: transferByPartition to the zero address should fail");
+		} else if (keccak256(statusCode) == keccak256(TRANSFER_FAILURE)) {
+			console2.log("TRANSFER_FAILURE");
+			bool validTransferFailure;
+			if (amount == 0 || partition != SHARED_SPACES_PARTITION) validTransferFailure = true;
+			assertTrue(validTransferFailure, "ERC1400: zero amount transferByPartition should fail");
+		} else if (keccak256(statusCode) == keccak256(INSUFFICIENT_BALANCE)) {
+			console2.log("INSUFFICIENT_BALANCE");
+			assertTrue(
+				ERC1400MockToken.balanceOfByPartition(SHARED_SPACES_PARTITION, from) < amount,
+				"ERC1400: insufficient balance transferByPartition should fail"
+			);
+		} else if (keccak256(statusCode) == keccak256(INSUFFICIENT_ALLOWANCE)) {
+			console2.log("INSUFFICIENT_ALLOWANCE");
+			assertTrue(
+				(amount > ERC1400MockToken.allowanceByPartition(SHARED_SPACES_PARTITION, from, to)),
+				"ERC1400: insufficient allowance transferByPartition should fail"
+			);
+		} else if (keccak256(statusCode) == keccak256(INVALID_DATA_OR_TOKEN_INFO)) {
+			console2.log("INVALID_DATA_OR_TOKEN_INFO");
+			assertTrue(data.length <= 10, "ERC1400: invalid data transferByPartition should fail");
+		} else {
+			console2.log("SUCCESS");
+			assertEq(
+				statusCode,
+				TRANSFER_SUCCESS,
+				"canTransferByPartition should return empty reason when all conditions are met"
+			);
+		}
+	}
 
 	function test_fuzz_canTransfer(address to, uint256 amount, bytes memory data) public {
 		vm.assume(to.code.length == 0);
@@ -108,5 +178,33 @@ abstract contract ERC1400CanTransferTest is ERC1400BaseTest {
 				"canTransferFrom should return empty reason when all conditions are met"
 			);
 		}
+	}
+
+	function testCanTransferFromContracts() public {
+		vm.startPrank(tokenAdmin);
+		ERC1400MockToken.approve(address(ERC1400ReceivableContract), 1000e18);
+		ERC1400MockToken.approve(address(nonERC1400ReceivableContract), 1000e18);
+		vm.stopPrank();
+
+		vm.startPrank(address(nonERC1400ReceivableContract));
+		(bool can, bytes memory reason, ) = ERC1400MockToken.canTransferFrom(
+			tokenAdmin,
+			address(nonERC1400ReceivableContract),
+			1000e18,
+			""
+		);
+
+		assertFalse(
+			can,
+			"canTransferFrom should return false when to is a contract that does not implement ERC1400Receiver"
+		);
+		assertEq(reason, INVALID_RECEIVER, "ERC1400: transfer to non ERC1400Receiver implementer should fail");
+
+		vm.startPrank(address(ERC1400ReceivableContract));
+		(can, reason, ) = ERC1400MockToken.canTransferFrom(tokenAdmin, address(ERC1400ReceivableContract), 1000e18, "");
+		vm.stopPrank();
+
+		assertTrue(can, "canTransfer should return true when to is a contract that implements ERC1400Receiver");
+		assertEq(reason, TRANSFER_SUCCESS, "canTransfer should return empty reason when all conditions are met");
 	}
 }
