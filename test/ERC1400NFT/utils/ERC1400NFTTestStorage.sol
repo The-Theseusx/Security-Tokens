0586dbe926884a3fa79b1ede61078681495d10cb
JayusJay <atayure@gmail.com>
2023-12-02 11:06:50 +0000

    QA

--- test/ERC1400NFT/utils/ERC1400NFTTestStorage.sol
+++ test/ERC1400NFT/utils/ERC1400NFTTestStorage.sol
@@ -89,7 +89,13 @@ abstract contract ERC1400NFTTestStorage is Test {
 	// Issuance / Redemption Events
 	event Issued(address indexed operator, address indexed to, uint256 tokenId, bytes data);
 	event Redeemed(address indexed operator, address indexed from, uint256 tokenId, bytes data);
-	event IssuedByPartition(bytes32 indexed partition, address indexed to, uint256 tokenId, bytes data);
+	event IssuedByPartition(
+		bytes32 indexed partition,
+		address indexed operator,
+		address indexed to,
+		uint256 tokenId,
+		bytes data
+	);
 	event RedeemedByPartition(
 		bytes32 indexed partition,
 		address indexed operator,
