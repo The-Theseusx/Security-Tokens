1cf30f6a8353961eaaf9980002bf039808d0d557
JayusJay <atayure@gmail.com>
2023-12-22 21:09:33 +0000

    chore: continued NFT redemption tests

--- test/ERC1400NFT/ERC1400NFTRedemption.t.sol
+++ test/ERC1400NFT/ERC1400NFTRedemption.t.sol
@@ -123,6 +123,10 @@ abstract contract ERC1400NFTRedemptionTest is ERC1400NFTBaseTest {
 	}
 
 	function testRedemptionShouldFailWhenCallerHasNoTokens() public {
+		///@dev issue two new tokens to tokenAdmin
+		vm.startPrank(tokenIssuer);
+		_issueTokens(DEFAULT_PARTITION, tokenAdmin, newTokenId_, "");
+		vm.stopPrank();
 		bytes memory validationData = prepareRedemptionSignature(
 			TOKEN_REDEEMER_PK,
 			DEFAULT_PARTITION,
@@ -136,6 +140,99 @@ abstract contract ERC1400NFTRedemptionTest is ERC1400NFTBaseTest {
 		vm.startPrank(notTokenAdmin);
 		vm.expectRevert("ERC1400NFT: Not token owner");
 		ERC1400NFTMockToken.redeem(newTokenId_, validationData);
+		vm.stopPrank();
+	}
+
+	function testRedeem() public {
+		bytes memory validationData = prepareRedemptionSignature(
+			TOKEN_REDEEMER_PK,
+			DEFAULT_PARTITION,
+			tokenAdmin,
+			ADMIN_INITIAL_TOKEN_ID,
+			0,
+			0
+		);
+
+		vm.startPrank(tokenAdmin);
+		ERC1400NFTMockToken.redeem(ADMIN_INITIAL_TOKEN_ID, validationData);
+		vm.stopPrank();
+
+		assertEq(ERC1400NFTMockToken.balanceOf(tokenAdmin), 0, "The tokenAdmin balance should be 0");
+
+		assertEq(
+			ERC1400NFTMockToken.balanceOfByPartition(DEFAULT_PARTITION, tokenAdmin),
+			0,
+			"The tokenAdmin default partition balance should be 0"
+		);
+
+		assertFalse(ERC1400NFTMockToken.exists(ADMIN_INITIAL_TOKEN_ID), "The tokenAdmin should no longer exist");
+	}
+
+	function testRedeemShouldFailWhenAlreadyRedeemed() public {
+		bytes memory validationData = prepareRedemptionSignature(
+			TOKEN_REDEEMER_PK,
+			DEFAULT_PARTITION,
+			tokenAdmin,
+			ADMIN_INITIAL_TOKEN_ID,
+			0,
+			0
+		);
+
+		vm.startPrank(tokenAdmin);
+		ERC1400NFTMockToken.redeem(ADMIN_INITIAL_TOKEN_ID, validationData);
+		vm.stopPrank();
+
+		assertFalse(ERC1400NFTMockToken.exists(ADMIN_INITIAL_TOKEN_ID), "The tokenAdmin should no longer exist");
+
+		validationData = prepareRedemptionSignature(
+			TOKEN_REDEEMER_PK,
+			DEFAULT_PARTITION,
+			tokenAdmin,
+			ADMIN_INITIAL_TOKEN_ID,
+			0,
+			0
+		);
+
+		vm.startPrank(tokenAdmin);
+		vm.expectRevert("ERC1400NFT: Token does not exist");
+		ERC1400NFTMockToken.redeem(ADMIN_INITIAL_TOKEN_ID, validationData);
+		vm.stopPrank();
+	}
+
+	function testErc1400NFTRedeemByPartition() public {
+		///@dev issue two new tokens to tokenAdmin
+		vm.startPrank(tokenIssuer);
+		_issueTokens(SHARED_SPACES_PARTITION, tokenAdmin, newTokenId_, "");
+		_issueTokens(SHARED_SPACES_PARTITION, tokenAdmin, newTokenId_ + 1, "");
+		vm.stopPrank();
+
+		///@notice used nonce 0
+		bytes memory validationData = prepareRedemptionSignature(
+			TOKEN_REDEEMER_PK,
+			SHARED_SPACES_PARTITION,
+			tokenAdmin,
+			newTokenId_,
+			0,
+			0
+		);
+
+		///@dev redeem ADMIN_INITIAL_TOKEN_ID
+		vm.startPrank(tokenAdmin);
+		ERC1400NFTMockToken.redeemByPartition(SHARED_SPACES_PARTITION, newTokenId_, validationData);
+		vm.stopPrank();
+
+		///@notice using nonce 1
+		bytes memory validationData2 = prepareRedemptionSignature(
+			TOKEN_REDEEMER_PK,
+			SHARED_SPACES_PARTITION,
+			tokenAdmin,
+			newTokenId_ + 1,
+			0,
+			0
+		);
+
+		vm.startPrank(tokenAdmin);
+		ERC1400NFTMockToken.redeemByPartition(SHARED_SPACES_PARTITION, newTokenId_ + 1, validationData2);
 		vm.stopPrank();
 	}
 }
