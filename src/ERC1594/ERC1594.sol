b77a748ef65a015dffcdb6a4aeb3b964cf357101
JayusJay <atayure@gmail.com>
2024-01-27 13:14:47 +0000

    chore!: revamped ERC1594

--- src/ERC1594/ERC1594.sol
+++ src/ERC1594/ERC1594.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import { ERC20 } from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import { Ownable2Step } from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+import { AccessControl } from "openzeppelin-contracts/contracts/access/AccessControl.sol";
 import { IERC1594 } from "./IERC1594.sol";
 import { EIP712 } from "openzeppelin-contracts/contracts/utils/cryptography/EIP712.sol";
 import { ECDSA } from "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
@@ -13,33 +14,63 @@ import { ECDSA } from "openzeppelin-contracts/contracts/utils/cryptography/ECDSA
  * @dev Utilizing ERC712 and ECDSA to validate data
  */
 
-contract ERC1594 is IERC1594, ERC20, EIP712, Ownable2Step {
-	/**
-	 * @dev EIP712 typehash for data validation
-	 */
+contract ERC1594 is IERC1594, ERC20, EIP712, AccessControl {
+	/// @dev EIP712 typehash for data validation
 	bytes32 public constant ERC1594_DATA_VALIDATION_HASH =
-		keccak256("ERC1594ValidateData(address from,address to,uint256 amount)");
+		keccak256("ERC1594ValidateData(address from,address to,uint256 amount,uint256 nonce,uint48 deadline)");
 
-	/**
-	 * @dev should track if token is issuable or not. Should not be modifiable if false.
-	 */
-	bool private _isIssuable = true;
+	///@dev Access control role for token admin.
+	bytes32 public constant ERC1594_ADMIN_ROLE = keccak256("ERC1594_ADMIN_ROLE");
 
-	/**
-	 * @dev event emitted when tokens are transferred with data attached
-	 */
+	///@dev Access control role for the token issuer.
+	bytes32 public constant ERC1594_ISSUER_ROLE = keccak256("ERC1594_ISSUER_ROLE");
+
+	///@dev Access control role for the token redeemer.
+	bytes32 public constant ERC1594_REDEEMER_ROLE = keccak256("ERC1594_REDEEMER_ROLE");
+
+	///@dev Access control role for the token transfer agent. Transfer agents can authorize transfers with their signatures.
+	bytes32 public constant ERC1594_TRANSFER_AGENT_ROLE = keccak256("ERC1594_TRANSFER_AGENT_ROLE");
+
+	///  @dev should track if token is issuable or not. Should not be modifiable if false.
+
+	bool private _isIssuable;
+
+	mapping(bytes32 => uint256) private _roleNonce;
+
+	/// @dev event emitted when tokens are transferred with data attached
 	event TransferWithData(address indexed from, address indexed to, uint256 amount, bytes data);
 
-	/**
-	 * @dev event emitted when issuance is disabled
-	 */
+	/// @dev event emitted when issuance is disabled
 	event IssuanceDisabled();
+
+	event NonceSpent(bytes32 indexed role, address indexed spender, uint256 nonceSpent);
+
+	error ERC1594_IssuanceDisabled();
+	error ERC1594_InvalidAddress(address addr);
+	error ERC1594_InvalidReceiver(address receiver);
+	error ERC1594_ZeroAmount();
+	error ERC1594_InvalidData();
+	error ERC1594_InvalidSignatureData();
 
 	constructor(
 		string memory name_,
 		string memory symbol_,
-		string memory version_
-	) ERC20(name_, symbol_) EIP712(name_, version_) {}
+		string memory version_,
+		address tokenAdmin,
+		address tokenIssuer,
+		address tokenRedeemer,
+		address tokenTransferAgent
+	) ERC20(name_, symbol_) EIP712(name_, version_) {
+		_setRoleAdmin(ERC1594_ADMIN_ROLE, ERC1594_ADMIN_ROLE);
+		_setRoleAdmin(ERC1594_ISSUER_ROLE, ERC1594_ADMIN_ROLE);
+		_setRoleAdmin(ERC1594_REDEEMER_ROLE, ERC1594_ADMIN_ROLE);
+		_setRoleAdmin(ERC1594_TRANSFER_AGENT_ROLE, ERC1594_ADMIN_ROLE);
+		_grantRole(ERC1594_ADMIN_ROLE, tokenAdmin);
+		_grantRole(ERC1594_ISSUER_ROLE, tokenIssuer);
+		_grantRole(ERC1594_REDEEMER_ROLE, tokenRedeemer);
+		_grantRole(ERC1594_TRANSFER_AGENT_ROLE, tokenTransferAgent);
+		_isIssuable = true;
+	}
 
 	/**
 	 * @dev See {IERC1594-isIssuable}.
@@ -48,11 +79,23 @@ contract ERC1594 is IERC1594, ERC20, EIP712, Ownable2Step {
 		return _isIssuable;
 	}
 
+	/// @return the nonce of a role.
+	function getRoleNonce(bytes32 role) public view virtual returns (uint256) {
+		return _roleNonce[role];
+	}
+
 	/**
 	 * @dev See {IERC1594-issue}.
 	 */
-	function issue(address tokenHolder, uint256 amount, bytes calldata data) public virtual override onlyOwner {
-		require(_isIssuable, "ERC1594: not issuable");
+	function issue(
+		address tokenHolder,
+		uint256 amount,
+		bytes calldata data
+	) public virtual override onlyRole(ERC1594_ISSUER_ROLE) {
+		if (!_isIssuable) revert ERC1594_IssuanceDisabled();
+		if (tokenHolder == address(0)) revert ERC1594_InvalidAddress(tokenHolder);
+		if (amount == 0) revert ERC1594_ZeroAmount();
+
 		_issue(tokenHolder, amount, data);
 	}
 
@@ -60,21 +103,34 @@ contract ERC1594 is IERC1594, ERC20, EIP712, Ownable2Step {
 	 * @dev See {IERC1594-redeem}.
 	 */
 	function redeem(uint256 amount, bytes calldata data) public virtual override {
-		_redeem(msg.sender, amount, data);
+		if (amount == 0) revert ERC1594_ZeroAmount();
+
+		_redeem(_msgSender(), amount, data);
 	}
 
 	/**
 	 * @dev See {IERC1594-redeemFrom}.
 	 */
-	function redeemFrom(address tokenHolder, uint256 amount, bytes calldata data) public virtual override onlyOwner {
-		_redeemFrom(tokenHolder, amount, data);
+	function redeemFrom(
+		address tokenHolder,
+		uint256 amount,
+		bytes calldata data
+	) public virtual override onlyRole(ERC1594_REDEEMER_ROLE) {
+		if (tokenHolder == address(0)) revert ERC1594_InvalidAddress(tokenHolder);
+		if (amount == 0) revert ERC1594_ZeroAmount();
+
+		_redeem(tokenHolder, amount, data);
 	}
 
 	/**
 	 * @dev See {IERC1594-transferWithData}.amount
 	 */
 	function transferWithData(address to, uint256 amount, bytes calldata data) public virtual override {
-		_transferWithData(msg.sender, to, amount, data);
+		if (to == address(0)) revert ERC1594_InvalidReceiver(to);
+		if (amount == 0) revert ERC1594_ZeroAmount();
+		if (data.length == 0) revert ERC1594_InvalidData();
+
+		_transferWithData(_msgSender(), to, amount, data);
 	}
 
 	/**
@@ -86,7 +142,12 @@ contract ERC1594 is IERC1594, ERC20, EIP712, Ownable2Step {
 		uint256 amount,
 		bytes calldata data
 	) public virtual override {
-		_transferFromWithData(from, to, amount, data);
+		if (from == address(0)) revert ERC1594_InvalidAddress(from);
+		if (to == address(0)) revert ERC1594_InvalidReceiver(to);
+		if (amount == 0) revert ERC1594_ZeroAmount();
+		if (data.length == 0) revert ERC1594_InvalidData();
+
+		_transferWithData(from, to, amount, data);
 	}
 
 	/**
@@ -97,10 +158,11 @@ contract ERC1594 is IERC1594, ERC20, EIP712, Ownable2Step {
 		uint256 amount,
 		bytes calldata data
 	) public view virtual override returns (bool, bytes memory, bytes32) {
-		if (balanceOf(msg.sender) < amount) return (false, bytes("0x52"), bytes32(0));
+		if (balanceOf(_msgSender()) < amount) return (false, bytes("0x52"), bytes32(0));
 		if (to == address(0)) return (false, bytes("0x57"), bytes32(0));
 		if (data.length != 0) {
-			if (_validateData(owner(), msg.sender, to, amount, data)) return (true, bytes("0x51"), bytes32(0));
+			(bool can, ) = _validateData(ERC1594_TRANSFER_AGENT_ROLE, _msgSender(), to, amount, data);
+			if (!can) return (false, bytes("0x57"), bytes32(0));
 		}
 		return (true, bytes("0x51"), bytes32(0));
 	}
@@ -114,64 +176,52 @@ contract ERC1594 is IERC1594, ERC20, EIP712, Ownable2Step {
 		uint256 amount,
 		bytes calldata data
 	) public view virtual override returns (bool, bytes memory, bytes32) {
-		if (amount > allowance(from, msg.sender)) return (false, bytes("0x53"), bytes32(0));
-		return canTransfer(to, amount, data);
+		if (amount > allowance(from, to)) return (false, bytes("0x53"), bytes32(0));
+		if (balanceOf(from) < amount) return (false, bytes("0x52"), bytes32(0));
+		if (to == address(0)) return (false, bytes("0x57"), bytes32(0));
+		if (data.length != 0) {
+			(bool can, ) = _validateData(ERC1594_TRANSFER_AGENT_ROLE, from, to, amount, data);
+			if (!can) return (false, bytes("0x57"), bytes32(0));
+		}
+		return (true, bytes("0x51"), bytes32(0));
 	}
 
 	/**
 	 * @dev issues tokens to a recipient
 	 */
 	function _issue(address tokenHolder, uint256 amount, bytes calldata data) internal virtual {
-		_beforeTokenTransferWithData(address(0), tokenHolder, amount, data);
-		if (data.length != 0) {
-			require(_validateData(owner(), address(0), tokenHolder, amount, data), "ERC1594: invalid data");
-		}
 		_mint(tokenHolder, amount);
-		emit Issued(msg.sender, tokenHolder, amount, data);
-		_afterTokenTransferWithData(address(0), tokenHolder, amount, data);
+		emit Issued(_msgSender(), tokenHolder, amount, data);
 	}
 
 	/**
 	 * @dev burns tokens from a recipient
 	 */
 	function _redeem(address from, uint256 amount, bytes calldata data) internal virtual {
-		_beforeTokenTransferWithData(from, address(0), amount, data);
-
-		if (data.length != 0) {
-			require(_validateData(owner(), from, address(0), amount, data), "ERC1594: invalid data");
+		if (data.length != 0 && !hasRole(ERC1594_REDEEMER_ROLE, _msgSender())) {
+			(bool authorized, address authorizer) = _validateData(
+				ERC1594_REDEEMER_ROLE,
+				from,
+				address(0),
+				amount,
+				data
+			);
+			if (!authorized) revert ERC1594_InvalidSignatureData();
+			_spendNonce(ERC1594_REDEEMER_ROLE, authorizer);
 		}
-
 		_burn(from, amount);
-		emit Redeemed(data.length == 0 ? msg.sender : owner(), from, amount, data);
-		_afterTokenTransferWithData(msg.sender, address(0), amount, data);
-	}
-
-	/**
-	 * @dev burns tokens from a recipient, to be called by an approved operator
-	 */
-	function _redeemFrom(address tokenHolder, uint256 amount, bytes calldata data) internal virtual {
-		_redeem(tokenHolder, amount, data);
+		emit Redeemed(_msgSender(), from, amount, data);
 	}
 
 	/**
 	 * @dev transfers tokens from a sender to a recipient with data
 	 */
 	function _transferWithData(address from, address to, uint256 amount, bytes calldata data) internal virtual {
-		_beforeTokenTransferWithData(from, to, amount, data);
-
-		require(_validateData(owner(), from, to, amount, data), "ERC1594: invalid data");
-
+		(bool authorized, address authorizer) = _validateData(ERC1594_TRANSFER_AGENT_ROLE, from, to, amount, data);
+		if (!authorized) revert ERC1594_InvalidSignatureData();
+		_spendNonce(ERC1594_TRANSFER_AGENT_ROLE, authorizer);
 		_transfer(from, to, amount);
-		emit TransferWithData(msg.sender, to, amount, data);
-		_afterTokenTransferWithData(from, to, amount, data);
-	}
-
-	/**
-	 * @dev transfers tokens from a sender to a recipient with data, to be called by an approved operator
-	 */
-	function _transferFromWithData(address from, address to, uint256 amount, bytes calldata data) internal virtual {
-		_spendAllowance(from, to, amount);
-		_transferWithData(from, to, amount, data);
+		emit TransferWithData(_msgSender(), to, amount, data);
 	}
 
 	/**
@@ -179,84 +229,40 @@ contract ERC1594 is IERC1594, ERC20, EIP712, Ownable2Step {
 	 * @param from address of the owner or authorized body
 	 * @param to address of the receiver
 	 * @param amount amount of tokens
-	 * @param signature data parameter
+	 * @param data data parameter
 	 */
 	function _validateData(
-		address authorizer,
+		bytes32 authorizerRole,
 		address from,
 		address to,
 		uint256 amount,
-		bytes calldata signature
-	) internal view virtual returns (bool) {
-		bytes32 structData = keccak256(abi.encodePacked(ERC1594_DATA_VALIDATION_HASH, from, to, amount));
-		bytes32 structDataHash = _hashTypedDataV4(structData);
-		address recoveredSigner = ECDSA.recover(structDataHash, signature);
+		bytes calldata data
+	) internal view virtual returns (bool, address authorizer) {
+		(bytes memory signature, uint48 deadline) = abi.decode(data, (bytes, uint48));
 
-		return recoveredSigner == authorizer;
+		bytes32 authorizerRole_ = authorizerRole;
+		bytes32 structData = keccak256(
+			abi.encodePacked(ERC1594_DATA_VALIDATION_HASH, from, to, amount, _roleNonce[authorizerRole_], deadline)
+		);
+		address recoveredSigner = ECDSA.recover(_hashTypedDataV4(structData), signature);
+
+		return (hasRole(authorizerRole_, recoveredSigner), recoveredSigner);
 	}
 
 	/**
 	 * @dev disables issuance of tokens, can only be called by the owner
 	 */
-	function disableIssuance() public virtual onlyOwner {
-		_disableIssuance();
-	}
-
-	/**
-	 * @dev renounce ownership and disables issuance of tokens
-	 */
-	function renounceOwnership() public virtual override onlyOwner {
-		_disableIssuance();
-		super.renounceOwnership();
-	}
-
-	/**
-	 * @dev intenal function to disable issuance of tokens
-	 */
-	function _disableIssuance() internal virtual {
+	function disableIssuance() public virtual {
 		_isIssuable = false;
 		emit IssuanceDisabled();
 	}
 
-	/**
-	 * @dev Hook that is called before any transfer of tokens with data. This includes
-	 * issuing and redeeming.
-	 *
-	 * Calling conditions:
-	 *
-	 * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-	 * will be transferred to `to`.
-	 * - when `from` is zero, `amount` tokens will be issued for `to`.
-	 * - when `to` is zero, `amount` of ``from``'s tokens will be redeemed.
-	 * - `from` and `to` are never both zero.
-	 *
-	 * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+	/*
+	 * @param role the role for which the nonce is increased
+	 * @param spender the address that spent the nonce in a signature
 	 */
-	function _beforeTokenTransferWithData(
-		address from,
-		address to,
-		uint256 amount,
-		bytes memory data
-	) internal virtual {}
-
-	/**
-	 * @dev Hook that is called after any transfer of tokens with data. This includes
-	 * issuing and redeeming.
-	 *
-	 * Calling conditions:
-	 *
-	 * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-	 * has been transferred to `to`.
-	 * - when `from` is zero, `amount` tokens have been issued for `to`.
-	 * - when `to` is zero, `amount` of ``from``'s tokens have been redeemed.
-	 * - `from` and `to` are never both zero.
-	 *
-	 * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-	 */
-	function _afterTokenTransferWithData(
-		address from,
-		address to,
-		uint256 amount,
-		bytes memory data
-	) internal virtual {}
+	function _spendNonce(bytes32 role, address spender) private {
+		uint256 nonce = ++_roleNonce[role];
+		emit NonceSpent(role, spender, nonce - 1);
+	}
 }
