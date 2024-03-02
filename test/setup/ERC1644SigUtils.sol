b3786b1172ad39b6a315a576ac6a8c8ee3bda4e5
JayusJay <atayure@gmail.com>
2024-03-02 16:42:17 +0000

    feat: setup ERC1594 and 1644 signature utils helpers

---
+++ test/setup/ERC1644SigUtils.sol
@@ -0,0 +1,78 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ERC1644TestStorage } from "./ERC1644TestStorage.sol";
+
+abstract contract ERC1644NFTSigUtils is ERC1644TestStorage {
+    struct PrepareSignatureParams {
+        uint256 signerPk;
+        bytes32 authorizerRole;
+        address from;
+        address to;
+        uint256 amount;
+        uint256 nonce;
+        uint48 deadline;
+    }
+
+    function prepareTransferSignature(
+        uint256 signerPk,
+        address from,
+        address to,
+        uint256 amount,
+        uint256 nonce,
+        uint48 deadline
+    ) public returns (bytes memory) {
+        bytes32 role = mockERC1644.ERC1594_TRANSFER_AGENT_ROLE();
+        nonce = nonce == 0 ? mockERC1644.getRoleNonce(role) : nonce;
+        deadline = deadline == 0 ? uint48(block.timestamp + 1 minutes) : deadline;
+
+        PrepareSignatureParams memory sigParams = PrepareSignatureParams({
+            signerPk: signerPk,
+            authorizerRole: role,
+            from: from,
+            to: to,
+            amount: amount,
+            nonce: nonce,
+            deadline: deadline
+        });
+
+        return _prepareSignature(sigParams);
+    }
+
+    function prepareRedemptionSignature(uint256 signerPk, address from, uint256 amount, uint256 nonce, uint48 deadline)
+        public
+        returns (bytes memory)
+    {
+        bytes32 role = mockERC1644.ERC1594_REDEEMER_ROLE();
+        nonce = nonce == 0 ? mockERC1644.getRoleNonce(role) : nonce;
+        deadline = deadline == 0 ? uint48(block.timestamp + 1 minutes) : deadline;
+
+        PrepareSignatureParams memory sigParams = PrepareSignatureParams({
+            signerPk: signerPk,
+            authorizerRole: role,
+            from: from,
+            to: address(0),
+            amount: amount,
+            nonce: nonce,
+            deadline: deadline
+        });
+
+        return _prepareSignature(sigParams);
+    }
+
+    function _prepareSignature(PrepareSignatureParams memory _sigParams) internal returns (bytes memory) {
+        ///@dev data field in ERC1400ValidateDataParams not needed in this step hence "" is used
+        bytes32 structMessage = sigUtilsContract.getERC1594TypedDataHash(
+            _sigParams.from, _sigParams.to, _sigParams.amount, _sigParams.nonce, _sigParams.deadline
+        );
+
+        address signer = vm.addr(_sigParams.signerPk);
+
+        vm.startPrank(signer);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_sigParams.signerPk, structMessage);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        vm.stopPrank();
+
+        return abi.encode(signature, _sigParams.deadline);
+    }
+}
