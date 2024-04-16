48f7db9bb87dffbfbc508e4cef0254273df0f75e
JayusJay <atayure@gmail.com>
2024-04-16 00:13:04 +0000

    chore!: setup ERC1594 tests

---
+++ test/ERC1594/ERC1594Test.t.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ERC1594IssuanceTest } from "./ERC1594IssuanceTest.t.sol";
+import { ERC1594RedemptionTest } from "./ERC1594RedemptionTest.t.sol";
+
+contract ERC1594Test is ERC1594IssuanceTest, ERC1594RedemptionTest { }
