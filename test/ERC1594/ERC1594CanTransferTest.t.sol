347db8b05b07c0b575e685ae5aacb52da9b3a5a3
JayusJay <atayure@gmail.com>
2024-06-01 12:04:44 +0000

    feat: Add ERC1594CanTransferTest contract as an abstract contract

---
+++ test/ERC1594/ERC1594CanTransferTest.t.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ERC1594BaseTest } from "./ERC1594BaseTest.t.sol";
+
+abstract contract ERC1594CanTransferTest is ERC1594BaseTest { }
